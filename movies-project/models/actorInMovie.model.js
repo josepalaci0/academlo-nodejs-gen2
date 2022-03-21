5944ddff0798f74d848482c4dcbae3e80c69459d
josepalaci0 <92130994+josepalaci0@users.noreply.github.com>
2022-03-21 08:37:36 -0500

    Update actorInMovie.model.js

--- movies-project/models/actorInMovie.model.js
+++ movies-project/models/actorInMovie.model.js
@@ -8,8 +8,14 @@ const ActorInMovie = sequelize.define('actorInMovie', {
     type: DataTypes.INTEGER,
     allowNull: false
   },
-  actorId: { type: DataTypes.INTEGER, allowNull: false },
-  movieId: { type: DataTypes.INTEGER, allowNull: false }
+  actorId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  movieId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
 });
 
 module.exports = { ActorInMovie };
